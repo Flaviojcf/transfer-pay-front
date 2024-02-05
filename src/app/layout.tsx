b817139c6b1e1b26c6fab744f961b266a4a45ce4
Flavio <fjcf@poli.br>
2024-02-05 17:19:37 -0300

    refactor: Change default layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,28 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
+import { Header } from './components/Header'
 
 export const metadata: Metadata = {
-  title: "TransferPay",
-  description: "Created by @FlavioJcf",
-};
+  title: 'TransferPay',
+  description: 'Created by @FlavioJcf',
+  icons: [
+    {
+      url: '/picpayLogo.png',
+    },
+  ],
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className="bg-customGreen">{children}</body> 
+      <body className="bg-customGreen">
+        <Header />
+        {children}
+      </body>
     </html>
-  );
+  )
 }
