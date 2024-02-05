27fe9373978c9a927b11409e30b46808c67cc8b6
Flavio <fjcf@poli.br>
2024-02-05 17:19:46 -0300

    feat: Create Loading

--- src/app/components/Loading/Loading.tsx
+++ src/app/components/Loading/Loading.tsx
@@ -1,23 +1,31 @@
 'use client'
 
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
 export function Loading({ children }: { children: React.ReactNode }) {
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
+      setIsLoading(false)
+    }, 2500)
 
-    return () => clearTimeout(timer);
-  }, []); 
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
-    <div className="flex">
-      {isLoading ? <Image className="object-contain" fill alt="Picpay loading" src="/picpay-loading.gif"/> : children}
+    <div className="flex ">
+      {isLoading ? (
+        <Image
+          className="object-contain -z-10"
+          fill
+          alt="Picpay loading"
+          src="/picpay-loading.gif"
+        />
+      ) : (
+        children
+      )}
     </div>
   )
 }
-
