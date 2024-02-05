61da9adfb0562a44f2db8b4213ad87e6a7d4eb1f
Flavio <fjcf@poli.br>
2024-02-05 17:18:24 -0300

    fix: Run lint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,13 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       screens: {
         '2xl': { max: '1535px' },
         // => @media (max-width: 1535px) { ... }
@@ -29,11 +24,12 @@ const config: Config = {
         sm: { max: '639px' },
         // => @media (max-width: 639px) { ... }
       },
-    },
-    colors: {
-      customGreen: 'rgb(9, 124, 73)',
+      colors: {
+        customGreen: 'rgb(9, 124, 73)',
+        customGray: '#bfc3b5',
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
