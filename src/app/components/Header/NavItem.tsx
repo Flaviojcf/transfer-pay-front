b9b734c9aa4346a8652389f6a4cef3dfb1f5a6a4
Flavio <fjcf@poli.br>
2024-02-05 17:18:53 -0300

    feat: Create NavItem

---
+++ src/app/components/Header/NavItem.tsx
@@ -0,0 +1,21 @@
+import { INavItem } from '@/app/interfaces/INavItem'
+import Link from 'next/link'
+import { ArrowUpRightFromSquare, Github, Linkedin } from 'lucide-react'
+
+export function NavItem({ href, label }: INavItem) {
+  return (
+    <Link href={href} className="flex items-center" target="blank">
+      <span className="flex items-center gap-2 text-lg text-black text-bold hover:opacity-40 transition-all ease-in-out">
+        {label === 'Linkedin' ? (
+          <Linkedin size={20} />
+        ) : label === 'Github' ? (
+          <Github size={20} />
+        ) : (
+          <ArrowUpRightFromSquare size={20} />
+        )}
+
+        {label}
+      </span>
+    </Link>
+  )
+}
---
+++ src/mock/navItens.ts
@@ -0,0 +1,14 @@
+export const NavItensLinks = [
+  {
+    href: 'https://www.linkedin.com/in/flávio-jcosta',
+    label: 'Linkedin',
+  },
+  {
+    href: 'https://github.com/Flaviojcf',
+    label: 'Github',
+  },
+  {
+    href: 'https://portfolio-flaviojcf.vercel.app/',
+    label: 'Portfólio',
+  },
+]
