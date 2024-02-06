d4571fe0a6bcea0627e7f4206fa8418a9e27be7c
Flavio <fjcf@poli.br>
2024-02-06 18:20:28 -0300

    refactor: Add mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,28 +14,48 @@ export default function Home() {
 
   return (
     <div className="mb-10">
-      <section className="flex items-center bg-customGray h-[750px] justify-between w-full ">
-        <div className="flex flex-col space-y-12 w-1/2 pl-64">
-          <h1 className="flex flex-wrap text-6xl text-[rgb(56, 50, 39)] font-bold ">
+      <section
+        className="flex items-center bg-customGray h-[750px] justify-between w-full 
+                lg:flex-col lg:pt-14
+                md:text-center  md:justify-around"
+      >
+        <div
+          className="flex flex-col space-y-12 w-1/2 pl-64
+             lg:w-full lg:items-center lg:px-4"
+        >
+          <h1
+            className="flex flex-wrap text-6xl text-[rgb(56, 50, 39)] font-bold 
+           lg:text-2xl"
+          >
             Muda pro PicPay e dê mais pique pro seu dinheiro
           </h1>
 
           <CustomButton
             className="flex space-x-4 items-center justify-center 
-          bg-emerald-600 rounded-lg w-60 h-12 text-white font-bold text-lg hover:opacity-75"
+          bg-emerald-600 rounded-lg w-60 h-12 text-white font-bold text-lg hover:opacity-75
+          lg:w-full"
           >
             <p>Abrir conta PicPay</p> <MoveRight />
           </CustomButton>
         </div>
 
-        <div className="flex w-1/2 overflow-hidden">
+        <div
+          className="flex w-1/2 overflow-hidden
+           lg:w-full"
+        >
           <Image src="/image.webp" width={952} height={750} alt="Picpay Ad" />
         </div>
       </section>
 
-      <section className="flex w-full pl-52 -mt-14 h-96 ">
-        <div className="flex items-center bg-[rgb(35,134,98)] w-full pl-14 space-x-14">
-          <div className="w-96 ">
+      <section
+        className="flex w-full pl-52 -mt-14 h-96 
+      lg:w-full lg:pl-0"
+      >
+        <div
+          className="flex items-center bg-[rgb(35,134,98)] w-full pl-14 space-x-14
+          md:pl-0 md:text-center md:items-center md:p-6 "
+        >
+          <div className="w-96 md:hidden">
             <Image
               src="/card.webp"
               width={400}
@@ -44,8 +64,14 @@ export default function Home() {
             ></Image>
           </div>
 
-          <div className="flex flex-col space-y-6  h-64 max-w-[680px]">
-            <h1 className="flex flex-wrap text-white text-5xl font-bold">
+          <div
+            className="flex flex-col space-y-6  max-w-[680px]
+            md:items-center"
+          >
+            <h1
+              className="flex flex-wrap text-white text-5xl font-bold 
+            md:text-2xl"
+            >
               PicPay Card: mais pique para suas compras
             </h1>
             <h2 className="flex flex-wrap text-white text-lg font-bold">
@@ -62,13 +88,18 @@ export default function Home() {
       <section
         className="flex items-center bg-white mt-16 pl-52 w-full justify-between  
         2xl:justify-normal 2xl:gap-16
-        lg:flex-col lg:pl-0 lg:items-center"
+        lg:flex-col lg:pl-0 lg:items-center
+        md:text-center
+        md:items-center"
       >
-        <div className="flex flex-col space-y-4 w-1/2 ">
-          <h1 className="text-5xl font-bold max-w-[500px]">
+        <div className="flex flex-col space-y-4 w-1/2  md:w-full md:items-center">
+          <h1
+            className="text-5xl font-bold max-w-[500px]
+          md:text-2xl md:w-full"
+          >
             Conta com o melhor rendimento do Brasil
           </h1>
-          <h2 className="text-lg max-w-[500px]">
+          <h2 className="text-lg max-w-[500px] md:w-full">
             Com PicPay, você facilita a sua vida, investindo, recebendo e
             pagando o que quiser de um só lugar, de forma segura e gratuita.
           </h2>
@@ -80,7 +111,8 @@ export default function Home() {
         <ul
           className="flex flex-wrap w-1/2 gap-8
         2xl:justify-center
-        lg:w-full"
+        lg:w-full
+        md:items-center"
         >
           {SectionItens.map((item) => (
             <li key={`item-${item.title}`} className="list-none ">
