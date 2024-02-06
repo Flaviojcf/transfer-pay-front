cb8c423a9420a3270c4b52bff0a659199b3fb0dd
Flavio <fjcf@poli.br>
2024-02-06 17:49:12 -0300

    feat: Add SectionItems mock

---
+++ src/mock/sectionItens.tsx
@@ -0,0 +1,88 @@
+export const SectionItens = [
+  {
+    image: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        fill="none"
+        viewBox="0 0 44 40"
+      >
+        <path
+          fill="#238662"
+          d="M15.12 28.339l.3.24a2 2 0 01.42.62 2 2 0 01-.42 2.18 2.919 2.919 0 01-.66.44 2 2 0 01-2.6-2.62 2.3 2.3 0 01.42-.66 2.001 2.001 0 011.82-.54l.36.16c.127.044.248.104.36.18z"
+        ></path>
+        <path
+          fill="#238662"
+          d="M1.757 1.756A6 6 0 016 0h16a6 6 0 015.982 5.53c.016.104.025.21.025.32v.026A2.003 2.003 0 0124.005 6H24a2 2 0 00-2-2H6a2 2 0 00-2 2v28a2 2 0 002 2h16a2 2 0 002-2 2.003 2.003 0 014.007.001v.027c0 .075-.004.148-.012.22A6 6 0 0122 40H6a6 6 0 01-6-6v-28a6 6 0 011.757-4.243z"
+        ></path>
+        <path
+          fill="#238662"
+          fillRule="evenodd"
+          d="M19.25 11.248A4.26 4.26 0 0122.263 10H39.74a4.26 4.26 0 014.261 4.26v11.48a4.261 4.261 0 01-4.26 4.26h-17.48a4.26 4.26 0 01-4.26-4.26V14.26a4.26 4.26 0 011.248-3.012zm21.086 15.086a.84.84 0 00.246-.595v-7.217h-19.16v7.217a.84.84 0 00.84.84h17.48a.84.84 0 00.594-.245zM21.668 13.666a.84.84 0 00-.246.595v.84h19.16v-.84a.84.84 0 00-.84-.84h-17.48a.841.841 0 00-.594.245z"
+          clipRule="evenodd"
+        ></path>
+      </svg>
+    ),
+    title: 'Conta completa',
+    description:
+      'PicPay dá mais pique pro seu dinheiro. Faça a portabilidade de salário para o PicPay e resolva a sua vida financeira do seu celular. ',
+  },
+  {
+    image: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="40"
+        height="41"
+        fill="none"
+        viewBox="0 0 40 41"
+      >
+        <path
+          fill="#238662"
+          d="M38 21h-6V3a2 2 0 00-3-1.74L23 4.7l-6-3.44a2 2 0 00-2 0L9 4.7 3 1.26A2 2 0 000 3v32a6 6 0 006 6h28a6 6 0 006-6V23a2 2 0 00-2-2zM6 37a2 2 0 01-2-2V6.46l4 2.28a2.16 2.16 0 002 0l6-3.44 6 3.44a2.16 2.16 0 002 0l4-2.28V35a6 6 0 00.36 2H6zm30-2a2 2 0 11-4 0V25h4v10z"
+        ></path>
+      </svg>
+    ),
+    title: 'Parcelamento de Pix e boletos',
+    description:
+      'Parcele Pix e boletos em até 12 vezes usando qualquer cartão de crédito e pague só na próxima fatura.',
+  },
+  {
+    image: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        fill="none"
+        viewBox="0 0 42 25"
+      >
+        <path
+          fill="#238662"
+          d="M40.844 1.24a2 2 0 00-1.08-1.08 2 2 0 00-.76-.16h-10a2 2 0 100 4h5.18l-11.18 11.18-6.58-6.6a2 2 0 00-2.84 0l-12 12a2 2 0 000 2.84 2 2 0 002.84 0l10.58-10.6 6.58 6.6a2 2 0 002.84 0l12.58-12.6V12a2 2 0 004 0V2a2 2 0 00-.16-.76z"
+        ></path>
+      </svg>
+    ),
+    title: 'Rendimento CDI',
+    description:
+      'O PicPay tem a conta com o melhor rendimento automático após 30 dias, sem limite máximo de investimento e com garantia FGC.',
+  },
+  {
+    image: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="44"
+        height="40"
+        fill="none"
+        viewBox="0 0 44 40"
+      >
+        <path
+          fill="#238662"
+          d="M18.92 8L20 6.82V14a2 2 0 004 0V6.82l1.08 1.1A2 2 0 0028 8a2 2 0 000-2.84L23.42.58a2 2 0 00-.66-.42 2 2 0 00-1.52 0 2 2 0 00-.66.42L16 5.08A2.065 2.065 0 1018.92 8zM22 20a6 6 0 100 12 6 6 0 000-12zm0 8a2 2 0 110-4 2 2 0 010 4zM8 26a2 2 0 104 0 2 2 0 00-4 0zm28 0a2 2 0 10-3.999 0A2 2 0 0036 26zm2-14h-8a2 2 0 000 4h8a2 2 0 012 2v16a2 2 0 01-2 2H6a2 2 0 01-2-2V18a2 2 0 012-2h8a2 2 0 000-4H6a6 6 0 00-6 6v16a6 6 0 006 6h32a6 6 0 006-6V18a6 6 0 00-6-6z"
+        ></path>
+      </svg>
+    ),
+    title: 'Conta das contas',
+    description:
+      'Movimente todas as suas contas, faça Pix com o saldo dos seus bancos, sem sair do PicPay. Organize todas as suas contas em um só app.',
+  },
+]
