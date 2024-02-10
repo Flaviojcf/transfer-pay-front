a2e63d79a28b0427c27a038d256d2d2816181c37
Flavio <fjcf@poli.br>
2024-02-10 14:48:40 -0300

    feat: Add shadcn data table

---
+++ src/app/(pages)/teste/page.tsx
@@ -0,0 +1,9 @@
+import Page from '@/app/components/Payments/page'
+
+export default function Teste() {
+  return (
+    <div className="flex h-screen border border-red-600  ">
+      <Page />
+    </div>
+  )
+}
