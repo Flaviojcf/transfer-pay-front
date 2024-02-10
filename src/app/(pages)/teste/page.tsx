091b42866f25bd931c92c85ff23c4875e09ca2b4
Flavio <fjcf@poli.br>
2024-02-10 15:08:13 -0300

    refactor: Add new filter

--- src/app/(pages)/teste/page.tsx
+++
@@ -1,9 +0,0 @@
-import Page from '@/app/components/Payments/page'
-
-export default function Teste() {
-  return (
-    <div className="flex h-screen border border-red-600  ">
-      <Page />
-    </div>
-  )
-}
